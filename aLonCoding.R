d80ca8960fa5b785bd1b1004fe154beb37c2948b
manhlambo <manhduc944@gmail.com>
2024-11-17 12:08:48 -0600

    update code

--- aLonCoding.R
+++ aLonCoding.R
@@ -4,27 +4,52 @@
 ##PART 1: DATA SELECTION
 #7,500 entries (cases/observations), 81 total columns (variables)
 
-#Select variables: Provider name 
-#Predictors: Number of Certified Beds (BEDCERT), Average Number of Residents per Day
-#Target: quality rating (1 or 5)
+#Select variables: 
+#Target: quality rating 5
 
 subset1<-myData[,c("PROVNAME", "quality_rating", "BEDCERT", "RESTOT",
                    "TOTHRD", "incident_cnt", "cmplnt_cnt", "PAYDEN_CNT", "TOT_PENLTY_CNT"
-                   ,"staffing_rating", "overall_rating", "staffing_rating")]
+                   ,"staffing_rating", "overall_rating", "survey_rating")]
 View(subset1)
-
-table(myData$staffing_rating)
+#Count the number of occurrences
+table(myData$quality_rating) 
 table(myData$overall_rating)
-table(myData$staffing_rating)
 
-##Inspect missing value
+#Inspect missing value
 missing_values<-colSums(is.na(subset1))
 View(missing_values) ##return quality rating has missing value
 
 length(missing_values[missing_values >0])
 
-subset1[!complete.cases(subset1), "quality_rating"]
+# Handle missing value using Omission Strategy -> Remove missing values
+subset1[!complete.cases(subset1),]
+omissionData<-na.omit(subset1)
+View(omissionData)
 
-##PART 2:MODEL BUILDING
+#convert quality_rating to binary
+omissionData$quality_rating<-ifelse(omissionData$quality_rating==5,1,0)
+table(omissionData$quality_rating) #return 5-stars: 2282, else: 5007
+
+subset2<-omissionData[,c("PROVNAME","quality_rating", "staffing_rating", "overall_rating", "survey_rating")]
+View(subset2)
+
+##PART 2:MODEL BUILDING - Classification model
 
 ##PART 3: MODEL COMPARISON AND 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
